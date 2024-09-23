5615fc24afc01decd6451dac4d92541b4afd4519
Siddharth Mishra <siddharth.mishra@flydocs.aero>
2024-09-23 05:59:10 +0530

    Step-2 Add skeleton routes for user

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+
+app.post("/user/signup", (req,res)=>{
+
+})
+
+app.post("/user/signin", (req,res)=>{
+    
+})
+
+
+app.get("/user/purchases", (req,res)=>{
+    
+})
+
+app.post("/course/purchase", (req,res)=>{
+    //right now we are not accepting payment scenerio
+    
+})
+
+app.get("/courses", (req,res)=>{
+    
+})
+
+app.listen(3000);
