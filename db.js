cfbf6a7a23842bf46cdc9c23698f09777d2e353d
Siddharth Mishra <siddharth.mishra@flydocs.aero>
2024-10-04 14:26:31 +0530

    Step-6 added the schema

---
+++ db.js
@@ -0,0 +1,46 @@
+const mongoose =require('mongoose');
+
+console.log("Connected to MongoDB");
+mongoose.connect("mongodb+srv://100xdevs:sidmish2309@cluster0.sj7uf.mongodb.net/coursera-app")
+
+const Schema=mongoose.Schema;
+const ObjectId=mongoose.Schema.Types.ObjectId;
+
+const userSchema=new Schema({
+    email:{type:String, unique:true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema=new Schema({
+    email:{type:String, unique:true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema=new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema=new Schema({
+    courseId: ObjectId,
+    userId: ObjectId,
+});
+
+const userModel=mongoose.model('user', userSchema);
+const adminModel=mongoose.model('admin', adminSchema);
+const courseModel=mongoose.model('course', courseSchema);
+const purchaseModel=mongoose.model('purchase', purchaseSchema);
+
+module.exports={
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}   
