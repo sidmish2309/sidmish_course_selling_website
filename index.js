ad7aa9f272b522fb2d9e206c09adb76441aa5a71
Siddharth Mishra <siddharth.mishra@flydocs.aero>
2024-10-04 13:15:06 +0530

    admin router added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const {userRouter}=require('./routes/user');
 const {courseRouter}=require('./routes/courses');
+const {adminRouter}=require('./routes/admin');
 
 const app=express();
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 
---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const {Router}=require('express');
+const adminRouter=Router();
+
+    adminRouter.post("/signup", (req,res)=>{
+        res.json({message:"admin created"});
+    })
+    
+    adminRouter.post("/signin", (req,res)=>{
+        res.json({message:"admin signed in"});
+    })
+    
+    adminRouter.post("/course", (req,res)=>{
+        res.json({message:"admin created a course"});
+    })
+
+    adminRouter.put("/course", (req,res)=>{
+        res.json({message:"admin updated a course"});
+    })
+
+    adminRouter.get("/course/bulk", (req,res)=>{
+        res.json({message:"admin sees all courses"});
+    })
+
+    module.exports={
+        adminRouter:adminRouter
+    }
