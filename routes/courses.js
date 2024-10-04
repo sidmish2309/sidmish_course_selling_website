714573c79404baefc06a8bbb8fd1112b798b26eb
Siddharth Mishra <siddharth.mishra@flydocs.aero>
2024-10-04 20:06:46 +0530

    step-13 Added course endpoints bussiness logics completed

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,29 @@
 const {Router}=require('express');
+const { userMiddleware } = require('../middleware/user');
 const courseRouter=Router();
+const {purchaseModel, courseModel}=require('../db');
 
-    courseRouter.post("/purchase", (req,res)=>{
+    courseRouter.post("/purchase", userMiddleware,async(req,res)=>{
         //right now we are not accepting payment scenerio
-        res.json({message:"Course purchased"});
+
+        const userId=req.userId;
+        const courseId=req.body.courseId;
+
+        await purchaseModel.create({
+            userId:userId,
+            courseId:courseId
+        });
+
+        res.json({message:"You have successfully purchased the course"});
     
     })
     
-    courseRouter.get("/preview", (req,res)=>{
-        res.json({message:"All Course preview"});
+    courseRouter.get("/preview", async(req,res)=>{
+
+        //This doesn't need to be authenticated
+
+        const courses=await courseModel.find({});
+        res.json({message:"All Course preview", courses});
     })
 
 
